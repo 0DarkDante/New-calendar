783d9ce1669a1a2447193c4bc93230a2ea94092f
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-12 22:36:19 +0200

    Create a table in the calendar

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
 let calendar = document.querySelector('#calendar');
 let body = calendar.querySelector('.body');
 
-let date  = new Date();
-let year  = date.getFullYear();
-let month = date.getMonth();
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1; // Добавляем 1, чтобы получить текущий месяц
 
 function range(count) {
-	let mas = [];
-  for(let i = 1; i <= count; i++) {
+  let mas = [];
+  for (let i = 1; i <= count; i++) {
     mas.push(i);
   }
   return mas;
 }
 
 function getLastDay(year, month) {
-  const date = new Date(year, month + 1, 0);
+  const date = new Date(year, month, 0);
   return date.getDate();
 }
 
 function getFirstWeekDay(year, month) {
-  const firstDayOfMonth = new Date(year, month, 1);
-  return firstDayOfMonth.getDay();
+  const firstDayOfMonth = new Date(year, month - 1, 1); // Вычитаем 1, чтобы учесть корректные индексы месяцев
+  let firstWeekDay = firstDayOfMonth.getDay();
+  return (firstWeekDay + 6) % 7; // Исправляем для правильного начала с понедельника
 }
 
 function getLastWeekDay(year, month) {
-  const lastDayOfMonth = new Date(year, month + 1, 0);
-  return lastDayOfMonth.getDay();
+  const lastDayOfMonth = new Date(year, month, 0);
+  let lastWeekDay = lastDayOfMonth.getDay();
+  return (lastWeekDay + 6) % 7; // Исправляем для правильного начала с понедельника
 }
+
+function normalize(arr, left, right) {
+  const normalizedArray = [...Array(left).fill(""), ...arr, ...Array(right).fill("")];
+  return normalizedArray;
+}
+
+function chunk(arr, n) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += n) {
+    result.push(arr.slice(i, i + n));
+  }
+  return result;
+}
+
+function createTable(parent, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let row = parent.insertRow();
+    for (let j = 0; j < arr[i].length; j++) {
+      let cell = row.insertCell();
+      cell.textContent = arr[i][j] || ""; // Добавляем проверку на пустые ячейки
+      if (arr[i][j] === date.getDate() && month === date.getMonth() + 1 && year === date.getFullYear()) {
+        cell.classList.add("today"); // Выделение ячейки для текущей даты
+      }
+    }
+  }
+}
+
+let arr = range(getLastDay(year, month));
+let firstWeekDay = getFirstWeekDay(year, month);
+let lastWeekDay = getLastWeekDay(year, month);
+
+let nums = chunk(normalize(arr, firstWeekDay, 6 - lastWeekDay), 7);
+let tableBody = document.querySelector('.body');
+
+createTable(tableBody, nums);

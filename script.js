9c4fe8248148ae71ce5d4e496c9a7db9aea2a9cd
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-12 22:02:05 +0200

    add main functions

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ let body = calendar.querySelector('.body');
 let date  = new Date();
 let year  = date.getFullYear();
 let month = date.getMonth();
+
+function range(count) {
+	let mas = [];
+  for(let i = 1; i <= count; i++) {
+    mas.push(i);
+  }
+  return mas;
+}
+
+function getLastDay(year, month) {
+  const date = new Date(year, month + 1, 0);
+  return date.getDate();
+}
+
+function getFirstWeekDay(year, month) {
+  const firstDayOfMonth = new Date(year, month, 1);
+  return firstDayOfMonth.getDay();
+}
+
+function getLastWeekDay(year, month) {
+  const lastDayOfMonth = new Date(year, month + 1, 0);
+  return lastDayOfMonth.getDay();
+}
